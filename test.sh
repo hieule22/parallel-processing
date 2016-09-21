18dd244ddcbdd8e8da96b50ad29f8054f25d3945
Hieu Le <htl5683@vh222004.truman.edu>
2016-09-21 12:05:08 -0500

    Increase number of tests

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 # First argument is executable binary to test.
 TARGET_PROG=$1
 # Number of tests to run.
-TEST_COUNT=9
+TEST_COUNT=10
 
 TEST_PROG=common_meeting_time_tester
 FILE_NAME=543GREbv.in
@@ -15,7 +15,7 @@ g++ -std=c++11 -o $TEST_PROG $TEST_PROG.cpp
 # Execute test program.
 for i in `seq 0 $TEST_COUNT`;
 do
-    ./$TEST_PROG w $i $FILE_NAME && ./$TARGET_PROG $FILE_NAME | ./$TEST_PROG t $i
+    ./$TEST_PROG -w $i $FILE_NAME && ./$TARGET_PROG $FILE_NAME | ./$TEST_PROG -t $i
 done
 
 # Clean up.
