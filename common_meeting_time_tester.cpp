fd6d409cab8ba8cfc76279b393779cbca842a1c5
Hieu Le <trunghieule94@gmail.com>
2016-09-20 23:26:10 -0500

    Reformat test representation

--- common_meeting_time_tester.cpp
+++ common_meeting_time_tester.cpp
@@ -9,16 +9,17 @@
  */
 class TestSuite {
 public:
+  /* Constructs a test case from specified input and output. */
   TestSuite(const std::vector<std::vector<int>>& input,
 	    const std::vector<int>& output);
 
-  /* Print test input to output stream. */
+  /* Print test input to specified stream. */
   void PrintInput(std::ostream& stream) const;
 
-  /* Print formatted test output to output stream. */
+  /* Print formatted test output to specified stream. */
   void PrintOutput(std::ostream& stream) const;
 
-  /* Check if output of given stream matches with expected test output. */
+  /* Check if content of specified stream agrees with expected test output. */
   bool Validate(std::istream& stream) const;
 
 private:
@@ -26,18 +27,21 @@ private:
   const std::vector<int> output_;  
 };
 
-/* Test cases. */
+/* Test cases are of the form. */
+/* {{1st schedule, 2nd schedule, 3rd schedule}, {Expected Output}}. */
 const TestSuite suites[] =
   {
-    {{{5, 3, 2, 4, 1, 5}, {1, 2}, {8, 9, 13, 17, 6, 5, 4, 3, 2}}, {2}},
-    {{{1, 1}, {1, 1}, {1, 1}}, {1}},
-    {{{4, 1, 2, 3, 4}, {4, 1, 2, 3, 4}, {4, 1, 2, 3, 4}}, {1, 2, 3, 4}},
-    {{{3, 1, 2, 3}, {3, 2, 3, 4}, {3, 3, 4, 5}}, {3}},
-    {{{2, 1, 2}, {4, 1, 2, 3, 4}, {4, 1, 2, 3, 4}}, {1, 2}},
-    {{{1, 1}, {1, 2}, {1, 3}}, {}},
-    {{{0}, {0}, {0}}, {}},
-    {{{0}, {1, 1}, {1, 1}}, {}},
-    {{{1, 1}, {1, 1}, {0}}, {}}
+    /*    FIRST                SECOND                   THIRD                       OUTPUT */
+    {{{5, 3, 2, 4, 1, 5},   {1, 2},            {8, 9, 13, 17, 6, 5, 4, 3, 2}},   {2}},
+    {{{1, 1},               {1, 1},            {1, 1}},                          {1}},
+    {{{4, 1, 2, 3, 4},      {4, 1, 2, 3, 4},   {4, 1, 2, 3, 4}},                 {1, 2, 3, 4}},
+    {{{3, 1, 2, 3},         {3, 2, 3, 4},      {3, 3, 4, 5}},                    {3}},
+    {{{2, 1, 2},            {4, 1, 2, 3, 4},   {4, 1, 2, 3, 4}},                 {1, 2}},
+    {{{3, 0, 999, 1000},    {2, 0, 999},       {2, 999, 99999}},                 {999}},
+    {{{1, 1},               {1, 2},            {1, 3}},                          {}},
+    {{{0},                  {0},               {0}},                             {}},
+    {{{0},                  {1, 1},            {1, 1}},                          {}},
+    {{{1, 1},               {1, 1},            {0}},                             {}}
   };
     
 TestSuite::TestSuite(const std::vector<std::vector<int>>& input,
@@ -46,7 +50,7 @@ TestSuite::TestSuite(const std::vector<std::vector<int>>& input,
 
 void TestSuite::PrintInput(std::ostream& stream) const {
   for (const auto& schedule : input_) {
-    for (int element : schedule) {
+    for (const int element : schedule) {
       stream << element << " ";
     }
     stream << std::endl;
@@ -82,13 +86,13 @@ bool TestSuite::Validate(std::istream& stream) const {
     actual.push_back(line);
   }
 
-  /* Sort to account for threads' asynchronocity. */
+  /* Sort to account for threads' asynchronicity. */
   std::sort(expected.begin(), expected.end());
   std::sort(actual.begin(), actual.end());
 
   if (expected.size() != actual.size()) {
-    std::cerr << "Expected: " << expected.size() << " elements." << std::endl;
-    std::cerr << "Actual: " << actual.size() << " elements." << std::endl;
+    std::cerr << "Expected: " << expected.size() << " lines." << std::endl;
+    std::cerr << "Actual: " << actual.size() << " lines." << std::endl;
     return false;
   }
 
@@ -108,9 +112,9 @@ int main(int argc, char *argv[]) {
   
   if (strcmp(argv[1], "w") == 0) {  /* Write to input file. */
     const std::string filename(argv[3]);
-    std::ofstream ofs(filename);
-    suites[test_number].PrintInput(ofs);
-    ofs.close();
+    std::ofstream out_stream(filename);
+    suites[test_number].PrintInput(out_stream);
+    out_stream.close();
   } else if (strcmp(argv[1], "t") == 0) {  /* Test output from program. */
     std::cerr << "Input #" << test_number << ":" << std::endl;
     suites[test_number].PrintInput(std::cerr);
