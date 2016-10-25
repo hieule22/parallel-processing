ff851e1b50ea053c47b363d1c7ce199e9156fc2c
Hieu Le <trunghieule94@gmail.com>
2016-10-24 23:19:19 -0500

    Fix failing tests for LGSMatcher

--- project2/java/ConcurrentPatternMatching/src/main/LimitedGapStringMatcher.java
+++ project2/java/ConcurrentPatternMatching/src/main/LimitedGapStringMatcher.java
@@ -1,46 +1,79 @@
 package main;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
- * LimitedGapStringMatcher determines how many times a limited gap string of
- * a given gap length is matched on an input String.
+ * LimitedGapStringMatcher determines how many times a limited gap string of a given gap length is
+ * matched on an input string.
  * @author Hieu Le
  * @version 10/23/16
  */
 public class LimitedGapStringMatcher {
-    // The format for the regular expression to be matched.
-    private static final String REGEX_FORMAT = "%s.{%d}%s";
-
-    // The pattern associated with this LimitedGapStringMatcher.
-    private final Pattern pattern;
+    // The prefix of the limited gap string.
+    private final String prefix;
+    // The suffix of the limited gap string.
+    private final String suffix;
+    // The number of characters permitted between prefix and suffix.
+    private final int gapLength;
 
     /**
-     * Constructs a LimitedGapStringMatcher from a limited gap string of
-     * given length.
+     * Constructs a LimitedGapStringMatcher from a limited gap string of given length.
      * @param prefix The prefix of the LGS
      * @param suffix The suffix of the LGS
-     * @param gapLength The number of characters permitted between prefix
-     *                  and suffix.
+     * @param gapLength The number of characters permitted between prefix and suffix
      */
     public LimitedGapStringMatcher(String prefix, String suffix, int gapLength) {
-        String regex = String.format(REGEX_FORMAT, prefix, gapLength, suffix);
-        pattern = Pattern.compile(regex);
+        if (prefix == null || suffix == null) {
+            throw new NullPointerException("Prefix and suffix cannot be null");
+        }
+        if (gapLength < 0) {
+            throw new IllegalArgumentException("Gap length must be non-negative: " + gapLength);
+        }
+        this.prefix = prefix;
+        this.suffix = suffix;
+        this.gapLength = gapLength;
     }
 
     /**
-     * Returns the number of times this limited gap string is matched on an
-     * input String.
-     * @param s The input String to match.
-     * @return The number of matches on s.
+     * Returns the number of times this limited gap string is matched on an input string.
+     * @param s The input string to match
+     * @return The number of matches on s
      */
     public int countMatch(String s) {
-        Matcher matcher = pattern.matcher(s);
+        if (s == null) {
+            throw new NullPointerException("s");
+        }
+        // TODO(hieule): Optimize this naive algorithm.
+        final int gapStringLength = prefix.length() + gapLength + suffix.length();
         int result = 0;
-        while (matcher.find()) {
-            result++;
+        for (int startIndex = 0; startIndex <= s.length() - gapStringLength; ++startIndex) {
+            final int endIndex = startIndex + gapStringLength;
+            if (areEqual(s, startIndex, prefix, 0, prefix.length()) &&
+                    areEqual(s, endIndex - suffix.length(), suffix, 0, suffix.length())) {
+                result++;
+            }
         }
         return result;
+    }
+
+    /**
+     * Checks if the substring of given length starting at index sStart of s is equal to the
+     * substring starting at index tStart of t.
+     * @param s the string containing the first substring
+     * @param sStart the starting index in s of the first substring
+     * @param t the string containing the second substring
+     * @param tStart the starting index in t of the second substring
+     * @param length the length of both substrings
+     * @return true if the two substrings are the same; false otherwise
+     */
+    private static boolean areEqual(String s, int sStart, String t, int tStart, int length) {
+        if (sStart + length > s.length() || tStart + length > t.length()) {
+            return false;
+        }
+
+        for (int offset = 0; offset < length; ++offset) {
+            if (s.charAt(sStart + offset) != t.charAt(tStart + offset)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- project2/java/ConcurrentPatternMatching/src/test/LimitedGapStringMatcherTest.java
+++ project2/java/ConcurrentPatternMatching/src/test/LimitedGapStringMatcherTest.java
@@ -4,6 +4,9 @@ import main.LimitedGapStringMatcher;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author Hieu Le
@@ -12,26 +15,52 @@ import static junit.framework.TestCase.assertEquals;
 public class LimitedGapStringMatcherTest {
 
     @Test
+    public void testConstructException() throws Exception {
+        try {
+            new LimitedGapStringMatcher("", "", -1);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException ex) {
+            assertThat(ex.getMessage(), containsString("Gap length must be non-negative"));
+        }
+
+        try {
+            new LimitedGapStringMatcher(null, "", 0);
+            fail("Expected a NullPointerException to be thrown");
+        } catch (NullPointerException ex) {
+            assertThat(ex.getMessage(), containsString("Prefix and suffix cannot be null"));
+        }
+    }
+
+    @Test
     public void testCountMatch() throws Exception {
-        {
-            LimitedGapStringMatcher matcher = new LimitedGapStringMatcher("Foo", "Bar", 1);
-            assertEquals(matcher.countMatch("Foo1Bar"), 1);
-            assertEquals(matcher.countMatch("Foo Bar"), 1);
-            assertEquals(matcher.countMatch("FooBar"), 0);
-            assertEquals(matcher.countMatch("FooQuozBar"), 0);
-        }
-        {
-            LimitedGapStringMatcher matcher = new LimitedGapStringMatcher("Foo", "Bar", 3);
-            assertEquals(matcher.countMatch("Foo123Bar"), 1);
-            assertEquals(matcher.countMatch("Foo   Bar"), 1);
-            assertEquals(matcher.countMatch("FooBar"), 0);
-            assertEquals(matcher.countMatch("Foo$Bar"), 0);
-            assertEquals(matcher.countMatch("Foo()Bar"), 0);
-        }
-        {
-            // TODO(hieule): Fix failing test case.
-//            LimitedGapStringMatcher matcher = new LimitedGapStringMatcher("Foo", "Foo", 2);
-//            assertEquals(matcher.countMatch("Foo12Foo12Foo"), 2);
+        LimitedGapStringMatcher matcher = new LimitedGapStringMatcher("Foo", "Bar", 1);
+        assertEquals(matcher.countMatch("Foo1Bar"), 1);
+        assertEquals(matcher.countMatch("Foo Bar"), 1);
+        assertEquals(matcher.countMatch("Foo1BarFoo2Bar"), 2);
+        assertEquals(matcher.countMatch("fooFoo Barbar"), 1);
+        assertEquals(matcher.countMatch("FooBar"), 0);
+        assertEquals(matcher.countMatch("FooQuozBar"), 0);
+        assertEquals(matcher.countMatch("foo bar"), 0);
+
+        matcher = new LimitedGapStringMatcher("Foo", "Bar", 3);
+        assertEquals(matcher.countMatch("Foo123Bar"), 1);
+        assertEquals(matcher.countMatch("Foo   Bar"), 1);
+        assertEquals(matcher.countMatch("FooBar"), 0);
+        assertEquals(matcher.countMatch("Foo$Bar"), 0);
+        assertEquals(matcher.countMatch("Foo()Bar"), 0);
+
+        matcher = new LimitedGapStringMatcher("Foo", "Foo", 2);
+        assertEquals(matcher.countMatch("Foo12Foo12Foo"), 2);
+        assertEquals(matcher.countMatch("Foo1Foo12Foo"), 1);
+    }
+
+    @Test
+    public void testCountMatchException() throws Exception {
+        try {
+            new LimitedGapStringMatcher("", "", 0).countMatch(null);
+            fail("Expected a NullPointerException to be thrown");
+        } catch (NullPointerException ex) {
+            assertThat(ex.getMessage(), containsString("s"));
         }
     }
 }
